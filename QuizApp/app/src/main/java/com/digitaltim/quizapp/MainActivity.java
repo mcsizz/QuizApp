ed05611cb0021d85d4f9a215dfd24534e0f63158
mcsizz <tmcwho@gmail.com>
2016-07-04 10:28:29 -0400

    Clean up
    Removed unused code and added proper comments

--- QuizApp/app/src/main/java/com/digitaltim/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/digitaltim/quizapp/MainActivity.java
@@ -1,12 +1,7 @@
 package com.digitaltim.quizapp;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -20,7 +15,6 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 1;
     int duration = Toast.LENGTH_SHORT;
 
     //@Override
@@ -46,24 +40,6 @@ public class MainActivity extends AppCompatActivity {
         EditText Q4Text = (EditText) findViewById(R.id.Q4_edit_text);
         String Q4String = Q4Text.getText().toString();
 
-
-//        RadioButton Q3A3 = (RadioButton) findViewById(R.id.Q3A3_radio_button);
-//        boolean Q3 = Q1A1.isChecked();
-//        RadioButton Q4A4 = (RadioButton) findViewById(R.id.Q4A4_radio_button);
-//        boolean Q4 = Q1A1.isChecked();
-//        RadioButton Q5A1 = (RadioButton) findViewById(R.id.Q5A1_radio_button);
-//        boolean Q5 = Q1A1.isChecked();
-//        RadioButton Q6A2 = (RadioButton) findViewById(R.id.Q6A2_radio_button);
-//        boolean Q6 = Q1A1.isChecked();
-//        RadioButton Q7A3 = (RadioButton) findViewById(R.id.Q7A3_radio_button);
-//        boolean Q7 = Q1A1.isChecked();
-//        RadioButton Q8A4 = (RadioButton) findViewById(R.id.Q8A4_radio_button);
-//        boolean Q8 = Q1A1.isChecked();
-//        RadioButton Q9A1 = (RadioButton) findViewById(R.id.Q9A1_radio_button);
-//        boolean Q9 = Q1A1.isChecked();
-//        RadioButton Q10A2 = (RadioButton) findViewById(R.id.Q10A2_radio_button);
-//        boolean Q10 = Q1A1.isChecked();
-
         // Get the name text
         EditText nameText = (EditText) findViewById(R.id.name_edit_text);
         String nameString = nameText.getText().toString();
@@ -74,20 +50,16 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreText = (TextView) findViewById(R.id.score);
         scoreText.setText(finalQuizMessage);
         Toast.makeText(this, finalQuizMessage, duration).show();
-
-//        Intent intent = new Intent(Intent.ACTION_SENDTO);
-//        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-//        intent.putExtra(Intent.EXTRA_SUBJECT, "Quiz score results for " + nameString);
-//        intent.putExtra(Intent.EXTRA_TEXT, finalQuizMessage);
-//        if (intent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(intent);
-//        }
     }
 
     /**
      * Calculates the price of the order.
      *
-     * @param Q1-Q10 are the radion buttons with the correct answers
+     * @param A1 radio button with single correct answer
+     * @param A2 radio button with single correct answer
+     * @param A3 checkbox with two correct answers
+     * @param A3_4 checkbox with two correct answers
+     * @param A4 string answer
      *
      * @return total score as a value of correct answers
      */
@@ -111,25 +83,6 @@ public class MainActivity extends AppCompatActivity {
         if (A4.equals("hypotenuse/adjacent")) {
             baseScore +=1;
         }
-
-//        if (A5) {
-//            baseScore += 1;
-//        }
-//        if (A6) {
-//            baseScore += 1;
-//        }
-//        if (A7) {
-//            baseScore += 1;
-//        }
-//        if (A8) {
-//            baseScore += 1;
-//        }
-//        if (A9) {
-//            baseScore += 1;
-//        }
-//        if (A10) {
-//            baseScore += 1;
-//        }
         return (baseScore);
     }
 
